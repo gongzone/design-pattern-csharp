07e612c3d1c24392bfbcc1b2f6f63896350c2277
WonYongJae <yongjae.won@im-fine.co.kr>
2023-12-01 23:50:03 +0900

    Builder Pattern

--- 04-Builder/Program.cs
+++ 04-Builder/Program.cs
@@ -1,8 +1,86 @@
 /* 
-* FactoryMethod (생성 패턴)
-* - 구체적으로 어떤 인스턴스를 만들지는 서브 클래스가 정한다.
-* - 다양한 구현체(Product)가 있고, 그 중에서 특정한 구현체를 만들 수 있는 다양한 팩토리(Creator)를
-*   제공할 수 있다.
+* Builder (생성 패턴)
+* - 동일한 프로세스를 거쳐 다양한 구성의 인스턴스를 만드는 방법.
+* - (복잡한) 객체를 만드는 프로세스를 독립적으로 분리할 수 있다.
 */
+
+interface IBuilder
+{
+    IBuilder BuildPartA();
+    IBuilder BuildPartB();
+    IBuilder BuildPartC();
+    Product getProduct();
+}
+
+class ConcreteBuilder : IBuilder
+{
+    private readonly string _partA;
+    private readonly string _partB; 
+    private readonly string _partC; 
+
+    public IBuilder BuildPartA(string partA)
+    {
+        _partA = partA;
+        return this;
+    }
+
+    public IBuilder BuildPartB(string partB)
+    {
+        _partB = partB;
+        return this;
+    }
+
+    public IBuilder BuildPartC(string partC)
+    {
+        _partC = partC;
+        return this;
+    }
+
+    public getProduct()
+    {
+        return new Product(_partA, _partB, _partC);
+    }
+}
+
+class Product
+{
+    public string PartA { get; set; }
+    public string PartB { get; set; }   
+    public string? PartC { get; set; }
+
+    public Product(string partA, string partB, string? partC)
+    {
+        PartA = partA;  
+        PartB = partB;
+        PartC = partC;  
+    }
+}
+
+class Director
+{
+    private readonly IBuilder _builder;
+
+    public Director(IBuilder builder) 
+    {
+        _builder = builder;
+    }
+
+    public Product CreateProductWithAllParts()
+    {
+        return _builder
+                .BuildPartA("partA")
+                .BuildPartB("partB")
+                .BuildPartC("partC")
+                .getProduct();
+    }
+
+    public Product CreateProductWithAAndB()
+    {
+        return _builder
+                .BuildPartA("partA")
+                .BuildPartB("partB")
+                .getProduct();
+    }
+}
 
 
