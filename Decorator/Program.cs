246f3ff3d1265875164f38ee4b2b18a163f1fc43
WonYongJae <yongjae.won@im-fine.co.kr>
2023-12-10 14:17:53 +0900

    Decorator Pattern

--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -1,5 +1,45 @@
 ﻿/* 
-* Adapter (구조 패턴)
-* - 기존 코드를 클라이언트가 사용하는 인터페이스의 구현체로 바꿔주는 패턴
-* - 클라이언트가 사용하는 인터페이스를 따르지 않는 기존 코드를 재사용할 수 있게 해준다.
+* Decorator (구조 패턴)
+* - 기존 코드를 변경하지 않고 부가 기능을 추가하는 패턴.
+* - 상속이 아닌 위임을 사용해서 보다 유연하게(런타임) 부가 기능을 추가하는 것도 가능하다.
 */
+
+public abstract class Component
+{
+    public abstract string Operation();
+}
+
+class ConcreteComponent : Component
+{
+    public override string Operation()
+    {
+        return "ConcreteComponent";
+    }
+}
+
+
+abstract class Decorator(Component component) : Component
+{
+    protected Component _component = component;
+
+    public override string Operation()
+    {
+        return _component.Operation();
+    }
+}
+
+class ConcreteDecoratorA(Component component) : Decorator(component)
+{
+    public override string Operation()
+    {
+        return $"ConcreteDecoratorA({base.Operation()})";
+    }
+}
+
+class ConcreteDecoratorB(Component component) : Decorator(component)
+{
+    public override string Operation()
+    {
+        return $"ConcreteDecoratorB({base.Operation()})";
+    }
+}
