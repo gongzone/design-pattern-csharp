5fa8b65d573ce2ca0692b2e8c933a8c01c5434e0
WonYongJae <yongjae.won@im-fine.co.kr>
2023-12-19 22:21:17 +0900

    Flyweight Pattern

--- Flyweight/Program.cs
+++ Flyweight/Program.cs
@@ -1,5 +1,43 @@
 ﻿/* 
-* Adapter (구조 패턴)
-* - 기존 코드를 클라이언트가 사용하는 인터페이스의 구현체로 바꿔주는 패턴
-* - 클라이언트가 사용하는 인터페이스를 따르지 않는 기존 코드를 재사용할 수 있게 해준다.
+* Flyweight (구조 패턴)
+* - 객체를 가볍게 만들어 메모리 사용을 줄이는 패턴
+* - 자주 변하는 속성 (또는 외적인 속성, extrinsit)과 변하지 않는 속성 (또는 내적인 속성, intrinsit)을
+*   분리하고 재사용하여 메모리 사용을 줄일 수 있다.
 */
+
+// Flyweight interface
+public interface ICharacter
+{
+    void Display();
+}
+
+// Concrete flyweight class
+public class Character(char symbol, int size, string font) : ICharacter
+{
+    private readonly char _symbol = symbol;
+    private readonly int _size = size;
+    private readonly string _font = font;
+
+    public void Display()
+    {
+        Console.WriteLine($"Symbol: {_symbol}, Size: {_size}, Font: {_font}");
+    }
+}
+
+// Flyweight factory class
+public class CharacterFactory
+{
+    private readonly Dictionary<string, ICharacter> _characters = new();
+
+    public ICharacter GetCharacter(char symbol, int size, string font)
+    {
+        var key = symbol.ToString() + size.ToString() + font;
+
+        if (!_characters.ContainsKey(key))
+        {
+            _characters.Add(key, new Character(symbol, size, font));
+        }
+
+        return _characters[key];
+    }
+}
